90f9372efa991d254426b4010964df2963273b70
Kristopher Ross <thekdr@yahoo.com>
2018-02-01 18:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newArray = (...kittens, name)
+  var newArray = [...kittens, name];
   return newArray;
 }
 
 function prependKitten(name) {
-  var newArray = (name, ...kittens)
+  var newArray = [name, ...kittens];
   return newArray;
 }
 
 function removeLastKitten() {
-  var newArray = kittens.slice(0, kittens.length - 1)
+  var newArray = kittens.slice(0, kittens.length - 1);
   return newArray;
 }
 
 function removeFirstKitten() {
-  var newArray = kittens.slice(1)
+  var newArray = kittens.slice(1);
   return newArray;
 }
