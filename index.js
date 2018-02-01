1f18653324227f54e4ec98821f0c09924207ac8f
Kristopher Ross <thekdr@yahoo.com>
2018-02-01 18:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newArray = (...kittens, name)
+  return newArray;
+}
+
+function prependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function removeLastKitten() {
+  return kittens.pop();
+}
+
+function removeFirstKitten() {
+  return kittens.shift();
+}
